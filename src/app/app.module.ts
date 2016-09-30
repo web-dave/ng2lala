df73a9318a689f313adffa4bc4e9d9503b3cbfbd
David Müllerchen <david.muellerchen@acando.de>
2016-09-30 11:22:04 +0200

    initial scaffolded app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
